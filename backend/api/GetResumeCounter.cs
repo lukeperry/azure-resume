bf65aa89bcc73b36c7eb7bd0ad56fe4c70d2ac66
Luke Perry Buenaventura <lukezabala@gmail.com>
2025-07-18 23:39:05 +0800

    Use dotnet publish for proper Azure Functions deployment

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -17,7 +17,7 @@ public class GetResumeCounter
     }
 
     [Function("GetResumeCounter")]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         
